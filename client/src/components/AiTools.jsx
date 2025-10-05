d28d65f986c157a6ac5b6bb38679dded9eda9676
mantosh7 <mahiraval000@gmail.com>
2025-10-06 02:10:04 +0530

    Ai Tools Landing Page Section

---
+++ client/src/components/AiTools.jsx
@@ -0,0 +1,16 @@
+import { AiToolsData } from '@/assets/assets'
+import React from 'react'
+
+const AiTools = () => {
+  return (
+    <div className='pt-30'>
+      <div className='text-center'>
+        <h1 className='text-4xl font-bold text-slate-300'>Ultimate AI Toolkit</h1>
+        <p className='mt-3 max-w-xl m-auto text-gray-500'>Craft smarter, better, and faster content with AI.</p>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default AiTools
