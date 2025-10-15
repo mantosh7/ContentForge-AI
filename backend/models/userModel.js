557a5f88e72ad5ca825d2a3c6d9a2b7767361085
mantosh7 <mahiraval000@gmail.com>
2025-10-16 03:42:54 +0530

    clean the commented code

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,24 +1,5 @@
 import sql from "../configs/db.js";
 
-// export async function createUsersTable() {
-//   try {
-//     await sql`
-//       CREATE TABLE IF NOT EXISTS users (
-//         id SERIAL PRIMARY KEY,
-//         full_name VARCHAR(100) NOT NULL,
-//         email VARCHAR(150) UNIQUE NOT NULL,
-//         password TEXT,
-//         google_id TEXT,
-//         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-//       );
-//     `;
-//     console.log("Users table exists or was created successfully");
-//   } catch (err) {
-//     console.error("Error creating users table:", err);
-//     throw err;
-//   }
-// }
-
 export async function insertUser({ full_name, email, password, google_id }) {
   const response = await sql`
     INSERT INTO users (full_name, email, password, google_id)
