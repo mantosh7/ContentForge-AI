38cd11fe0be1b093ba656823600c0114b8fb8f10
mantosh7 <mahiraval000@gmail.com>
2025-10-16 03:41:54 +0530

    clean commented code

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
-// import { createUsersTable } from "./models/userModel.js";
 
 dotenv.config(); 
 
@@ -15,18 +14,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Server is live...");
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`)
-
-  // try {
-  //   await createUsersTable(); 
-  //   console.log("Users table checked/created successfully");
-  // } catch (err) {
-  //   console.error("Error creating users table:", err);
-  // }
 });
