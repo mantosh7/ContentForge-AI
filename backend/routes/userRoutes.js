2a8725cd50c573787066c1a196b9ecef945df0af
mantosh7 <mahiraval000@gmail.com>
2025-10-20 05:56:17 +0530

    User Routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import express from "express" ;
+import { authenticateToken } from "../middlewares/authMiddleware.js";
+import { getUserCreations } from "../controllers/userController.js";
+
+const router = express.Router() ;
+
+router.get("/get-user-creations", authenticateToken, getUserCreations) ;
+
+export default router ;
